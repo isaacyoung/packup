47483da07e13959a257d63052cdf20a68a24c12f
yzh <48515824@qq.com>
2018-06-08 10:18:53 +0800

    other

--- src/main/kotlin/cn/isaac/customer/isaac/user.kt
+++ src/main/kotlin/cn/isaac/customer/isaac/user.kt
@@ -5,56 +5,52 @@ import cn.isaac.config.configBuilder
 import cn.isaac.manage.PackUpManager
 import cn.isaac.tools.Diff
 
-/**
- *
- * @author yzh
- * @date 2018/5/30
- */
-
 fun main(args: Array<String>) {
     config = configBuilder {
 
         // 项目路径
-        projectPath = "E:\\git\\zhhq"
+        projectPath = "E:/yunma/user"
 
         // 编译环境 eclipse or idea
         // eclipse 编译后的文件路径须在/WebRoot/WEB-INF/classes
         ide = "idea"
 
         // 主项目 ide=idea有效
-        mainProject = "LIANLIAN_WXSCHOOL_LOGISTICS"
+        mainProject = "LIANLIAN_USER"
 
         // 输出路径
         targetPath = "F:/output"
 
-        // 版本 none git svn
-        versionControl = "git"
-        beginGitNum = "0862896e7329e064d90b28495862e6294dd7373d"
-        endGitNum = "159e88f046850ae7d0794bb8b8f7ac14f186f869"
+        // 是否根据svn版本号获取。false：根据文件修改时间获取
+        versionControl = "svn"
 
         // 获取修改时间之后的变动文件
-        fromDate = "2018-01-11 16:30:00"
+        fromDate = "2018-06-06 10:00:00"
 
         // svn账号
         svnUser = "yangzh"
         svnPassword = "123456"
 
-        svn("http://192.168.72.13/svn/logistics/source/trunk/") {
-            add(1613)
+        svn("http://192.168.72.13/svn/LSMART/LIANLIAN_ZHIHUI/trunck/source/") {
+            add(38246)
+            add(38247)
         }
 
     }
 
-//    PackUpManager.run()
+    PackUpManager.run()
 
 
     fun diff() {
-        val project = "YUNMA_JIAOFEI_SERVICE"
-        val oldFile = "F:\\LIANLIAN_DAYLY\\生产环境\\升级申请\\升级文件列表\\20180604缴费大厅app\\$project.war.zip"
+        val project = "LIANLIAN_WXSCHOOL_LOGISTICS_SERVICE"
+        val oldFile = "F:\\LIANLIAN_DAYLY\\生产环境\\升级申请\\升级文件列表\\20180420广告点击统计\\$project.war.zip"
         val newFile = "F:\\output\\$project.war.zip"
 
         val diff = Diff(oldFile,newFile)
         diff.run()
     }
-    diff()
+//    diff()
+
+
 }
+
